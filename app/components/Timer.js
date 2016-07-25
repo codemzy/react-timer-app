db4b08db6af2b4537fe0f23da2f3d22dddacbc31
Emma J <codemzy@gmail.com>
2016-07-25 12:13:17 +0000

    Updated Timer component and wired to Clock and Controls

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -1,11 +1,64 @@
 var React = require('react');
 
-var Timer = (props) => {
-    return (
-        <div>
-            <h1 className="text-center page-title">This is the Timer.</h1>
-        </div>
-    ); 
-};
+// components
+var Clock = require('./Clock');
+var Controls = require('./Controls');
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+            countdownStatus: 'paused'
+        };
+    }
+    
+    // listen for state change
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            if (this.state.countdownStatus === 'started') {
+                this._startTimer();
+            } else if (this.state.countdownStatus === 'stopped') {
+                this.setState({count: 0, countdownStatus: 'paused'});
+                clearInterval(this.timer);
+            } else if (this.state.countdownStatus === 'paused') {
+                clearInterval(this.timer);
+            }
+        }
+    }
+    
+    // clear the interval if changing pages
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+    
+    _startTimer() {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count + 1;
+            this.setState({
+                count: newCount 
+            });
+            if (newCount === 3600) {
+                this.setState({countdownStatus: 'stopped'});
+            }
+        }, 1000);
+    }
+    
+    _handleStatusChange(newStatus) {
+        this.setState({
+            countdownStatus: newStatus
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1 className="text-center page-title">Timer</h1>
+                <Clock totalSeconds={this.state.count} />
+                <Controls countdownStatus={this.state.countdownStatus} onStatusChange={this._handleStatusChange.bind(this)} />
+            </div>
+        ); 
+    }
+}
 
 module.exports = Timer;
